3091ca9daf4d102c51fd58e5ebf37b80c5da6476
knpatil <patilkam@yahoo.com>
2020-07-31 16:56:43 -0700

    vehicles api - starter code

---
+++ vehicles-api/src/main/java/com/kpatil/vehicles/domain/car/CarRepository.java
@@ -0,0 +1,9 @@
+package com.kpatil.vehicles.domain.car;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarRepository extends JpaRepository<Car, Long> {
+
+}
