dbaab39e685dbf28afb9b11efd7f2bd19e6f7ed7
knpatil <patilkam@yahoo.com>
2020-07-30 23:29:49 -0700

    added bootstrap.properties

--- pricing-service/src/main/java/com/kpatil/pricing/entity/Price.java
+++ pricing-service/src/main/java/com/kpatil/pricing/entity/Price.java
@@ -19,15 +19,22 @@ public class Price {
 
     private String currency;
     private BigDecimal price;
-    private Long vehicleId;
 
     public Price() {
     }
 
-    public Price(String currency, BigDecimal price, Long vehicleId) {
+    public Price(Long id, String currency, BigDecimal price) {
+        this.id = id;
         this.currency = currency;
         this.price = price;
-        this.vehicleId = vehicleId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getCurrency() {
@@ -44,13 +51,5 @@ public class Price {
 
     public void setPrice(BigDecimal price) {
         this.price = price;
-    }
-
-    public Long getVehicleId() {
-        return vehicleId;
-    }
-
-    public void setVehicleId(Long vehicleId) {
-        this.vehicleId = vehicleId;
     }
 }
